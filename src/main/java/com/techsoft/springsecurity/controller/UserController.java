900cb381b558545f9c8bf33f00de20361d5d8db4
mukeshkrsingh <chandan8520@gmail.com>
2024-05-09 11:11:39 +0530

    first commit;

---
+++ src/main/java/com/techsoft/springsecurity/controller/UserController.java
@@ -0,0 +1,97 @@
+package com.techsoft.springsecurity.controller;
+
+import com.techsoft.springsecurity.entity.AuthRequest;
+import com.techsoft.springsecurity.entity.UserInfo;
+import com.techsoft.springsecurity.logout.BlackList;
+import com.techsoft.springsecurity.service.JwtService;
+import com.techsoft.springsecurity.service.UserInfoService;
+import jakarta.servlet.http.HttpServletRequest;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.access.prepost.PreAuthorize;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+import java.util.Objects;
+
+@RestController
+@RequestMapping("/auth")
+public class UserController {
+    @Autowired
+    private UserInfoService userInfoService;
+    @Autowired
+    private AuthenticationManager authenticationManager;
+    @Autowired
+    private JwtService jwtService;
+
+    @Autowired
+    private BlackList blackList;
+
+    @GetMapping("/welcome")
+    public String welcome(){
+        return "Welcome to Spring Security tutorials !!";
+    }
+
+    @PostMapping("/addUser")
+    public String addUser(@RequestBody UserInfo userInfo){
+        return userInfoService.addUser(userInfo);
+    }
+    @PostMapping("/login")
+    public String addUser(@RequestBody AuthRequest authRequest){
+        Authentication authenticate = authenticationManager.authenticate(new UsernamePasswordAuthenticationToken(authRequest.getUserName(), authRequest.getPassword()));
+        if(authenticate.isAuthenticated()){
+            return jwtService.generateToken(authRequest.getUserName());
+        }else {
+            throw new UsernameNotFoundException("Invalid user request");
+        }
+    }
+    @PostMapping("/logout")
+    @PreAuthorize("hasAuthority('USER_ROLES') or hasAuthority('ADMIN_ROLES')")
+    public String logoutUser(HttpServletRequest request){
+        String authHeader = request.getHeader("Authorization");
+        String token= null;
+        if(authHeader !=null && authHeader.startsWith("Bearer")){
+            token = authHeader.substring(7);
+        }
+        blackList.blacKListToken(token);
+        return "You have successfully logged out !!";
+    }
+
+    @GetMapping("/getUsers")
+    @PreAuthorize("hasAuthority('ADMIN_ROLES') or hasAuthority('USER_ROLES')")
+    public List<UserInfo> getAllUsers(){
+        return userInfoService.getAllUser();
+    }
+    @GetMapping("/getUsers/{id}")
+    @PreAuthorize("hasAuthority('USER_ROLES')")
+    public UserInfo getAllUsers(@PathVariable Integer id){
+        return userInfoService.getUser(id);
+    }
+    
+    @PutMapping("/update")
+    @PreAuthorize("hasAuthority('ADMIN_ROLES')")
+    public String update(@RequestBody UserInfo info) {
+    	
+    		 UserInfo i =  userInfoService.getUserByName(info.getName());
+    		 if (Objects.nonNull(info.getName().toLowerCase())
+    		            && !"".equalsIgnoreCase(
+    		                i.getName().toLowerCase())) {
+    			 i.setName(info.getName().toLowerCase());
+    			 i.setPassword(info.getPassword());
+    			 userInfoService.addUser(i);
+    			 return "User updated successfully";
+    		        }else {
+    		        	 throw new UsernameNotFoundException("Invalid user request");
+    		        }
+             
+         
+    	
+    	
+    }
+    
+    
+    
+}
---
+++ src/main/java/com/techsoft/springsecurity/entity/UserInfo.java
@@ -0,0 +1,54 @@
+package com.techsoft.springsecurity.entity;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import lombok.AllArgsConstructor;
+import lombok.NoArgsConstructor;
+
+@Entity
+@AllArgsConstructor
+@NoArgsConstructor
+public class UserInfo {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer id;
+    private String name;
+    private String email;
+    private String roles;
+    private String password;
+	public Integer getId() {
+		return id;
+	}
+	public void setId(Integer id) {
+		this.id = id;
+	}
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public String getEmail() {
+		return email;
+	}
+	public void setEmail(String email) {
+		this.email = email;
+	}
+	public String getRoles() {
+		return roles;
+	}
+	public void setRoles(String roles) {
+		this.roles = roles;
+	}
+	public String getPassword() {
+		return password;
+	}
+	public void setPassword(String password) {
+		this.password = password;
+	}
+    
+    
+    
+}
